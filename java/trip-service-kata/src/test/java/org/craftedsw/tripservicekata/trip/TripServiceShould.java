3b8f329d95d86777e90cfdc129b0e1e016fa45be
Toby Retallick <toby.retallick@gmail.com>
2014-09-15 13:26:31 +0100

    TripService returns empty trips array if logged in user is not friends with the other user

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -2,18 +2,34 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class TripServiceShould {
 
     private static final User GUEST = null;
     private static final User SOME_USER = new User();
+    private static final User USER_WITH_ACCOUNT = new User();
+    private List<Trip> tripArray;
+    private static final Trip TRIP1 = new Trip();
+    private static final Trip TRIP2 = new Trip();
     private TripService tripService;
     private User loggedInUser;
+    private List<User> someUserFriends;
+
+    @Before
+    public void initialise() {
+        tripService = new TestableTripService();
+
+    }
 
     @Test(expected = UserNotLoggedInException.class) public void
     should_validate_the_logged_in_user() {
-        this.tripService = new TestableTripService();
 
         loggedInUser = GUEST;
 
@@ -25,6 +41,12 @@ public class TripServiceShould {
         protected User loggedInUser() {
             return loggedInUser;
         }
+    }
+
+    @Test public void
+    return_empty_tripList_if_logged_in_user_is_not_friends_with_another_user() {
+        loggedInUser = USER_WITH_ACCOUNT;
+        assertThat(tripService.getTripsByUser(SOME_USER).isEmpty(), is(true));
     }
 
 }
