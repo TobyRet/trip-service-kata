a5c952a052d21be5ab1e73adda90bda1b57bffda
Toby Retallick <toby.retallick@gmail.com>
2014-09-16 10:32:10 +0100

    Test validates a logged in user

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,28 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
 public class TripServiceTest {
+
+    private static final User SOME_USER = new User();
+    private TripService tripService;
+    private User loggedInUser;
+    private User GUEST = null;
+
+    @Test(expected = UserNotLoggedInException.class) public void
+    validates_the_logged_in_user() {
+        loggedInUser = GUEST;
+        tripService = new TestableTripService();
+        tripService.getTripsByUser(SOME_USER);
+    }
+
+    private class TestableTripService extends TripService {
+        @Override
+        protected User loggedInUser() {
+            return loggedInUser;
+        }
+    }
 	
 }
