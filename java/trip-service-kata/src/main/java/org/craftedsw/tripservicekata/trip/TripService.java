d630e3db61b3d5f1c8a1d5bd2d16bd33b752fbfe
Toby Retallick <toby.retallick@gmail.com>
2014-09-16 12:32:12 +0100

    Refactored - loggedInUser passed in as parametre getTripsByUser

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -2,28 +2,23 @@ package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
-import org.craftedsw.tripservicekata.user.UserSession;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TripService {
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		if (loggedInUser() == null) {
+	public List<Trip> getTripsByUser(User user, User loggedInUser) throws UserNotLoggedInException {
+		if (loggedInUser == null) {
             throw new UserNotLoggedInException();
         }
-		return user.isFriendsWith(loggedInUser())
+		return user.isFriendsWith(loggedInUser)
                 ? tripsByUser(user)
                 : new ArrayList<Trip>();
 	}
 
     protected List<Trip> tripsByUser(User user) {
         return TripDAO.findTripsByUser(user);
-    }
-
-    protected User loggedInUser() {
-        return UserSession.getInstance().getLoggedUser();
     }
 
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -23,48 +23,41 @@ public class TripServiceTest {
 
     @Before
     public void initialise() {
+        loggedInUser = REGISTERED_USER;
         testableTripService = new TestableTripService();
     }
 
     @Test(expected = UserNotLoggedInException.class) public void
     validates_the_logged_in_user() {
         loggedInUser = GUEST;
-        testableTripService.getTripsByUser(SOME_USER);
+        testableTripService.getTripsByUser(SOME_USER, GUEST );
     }
 
     @Test public void
     return_an_empty_list_if_user_logged_in_and_not_a_friend() {
-        loggedInUser = REGISTERED_USER;
 
         User stranger = UserBuilder.aUser()
                             .friendsWith(ANOTHER_USER)
                             .withTripsTo(THAILAND)
                             .build();
 
-        List<Trip> trips = testableTripService.getTripsByUser(stranger);
+        List<Trip> trips = testableTripService.getTripsByUser(stranger, REGISTERED_USER);
         assertThat(trips.isEmpty(), is(true));
     }
 
     @Test public void
     return_a_populated_trips_list_if_user_is_logged_in_and_a_friend() {
-        loggedInUser = REGISTERED_USER;
-
         User friend = UserBuilder.aUser()
                             .friendsWith(ANOTHER_USER, REGISTERED_USER)
                             .withTripsTo(THAILAND, ICELAND)
                             .build();
 
-        List<Trip> trips = testableTripService.getTripsByUser(friend);
+        List<Trip> trips = testableTripService.getTripsByUser(friend, REGISTERED_USER);
         assertThat(trips.isEmpty(), is(false));
         assertThat(trips.size(), is(2));
     }
 
     private class TestableTripService extends TripService {
-        @Override
-        protected User loggedInUser() {
-            return loggedInUser;
-        }
-
         @Override
         protected List<Trip> tripsByUser(User user) {
             return user.trips();
